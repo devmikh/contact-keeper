3e8c68a5d4f6e27ccdaf8b44b215760b881af05e
Mikhail Serebriakov <mishacyb@gmail.com>
2019-09-28 16:58:54 -0700

    Added user registration and authentication

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
 
 // @route     GET api/auth
 // @desc      Get logged in user
@@ -11,8 +17,64 @@ router.get("/", (req, res) => {
 // @route     POST api/auth
 // @desc      Auth user & get token
 // @access    Public
-router.post("/", (req, res) => {
-  res.send("Log in user");
-});
+router.post(
+  "/",
+  [
+    // Check request body for possible errors
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  async (req, res) => {
+    // If any errors exist, show them in the response
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Pull out variables from request body
+    const { email, password } = req.body;
+
+    try {
+      // Find user with same email
+      let user = await User.findOne({ email });
+
+      // If there's no such user, respond with this message
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid Credentials" });
+      }
+
+      // Compare entered password with password in a db and assign the boolean value to isMatch variable
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      // If passwords don't match, respond with this message
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid Credentials" });
+      }
+
+      // Create payload which includes user's id
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      // Create json web token and send it as a response
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
@@ -10,6 +13,7 @@ const User = require("../models/User");
 router.post(
   "/",
   [
+    // Check request body for possible errors
     check("name", "Please add a name")
       .not()
       .isEmpty(),
@@ -19,12 +23,64 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
+    // If any errors exist, show them in the response
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("Passed");
+
+    // Pull out variables from request body
+    const { name, email, password } = req.body;
+
+    try {
+      // Find user with same email
+      let user = await User.findOne({ email });
+
+      // If such user exists, respond with this message
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+
+      // If such user doesn't exists, create new user with these credentials
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      // Create salt for hashing
+      const salt = await bcrypt.genSalt(10);
+
+      // Assign encrypted password instead of the literal one
+      user.password = await bcrypt.hash(password, salt);
+
+      // Add user to the db
+      await user.save();
+
+      // Create payload which includes user's id
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      // Create json web token and send it as a response
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
